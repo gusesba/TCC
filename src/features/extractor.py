300cb36e061213fa3aa11c643a0171f5ba22d458
Gustavo Esmanhotto Bareta <gustavo.bareta@mps.interno>
2025-08-06 17:30:35 -0300

    lista com todas as caracteristicas

--- src/features/extractor.py
+++ src/features/extractor.py
@@ -1,13 +1,16 @@
 from tsfresh import extract_features
+from tsfresh.feature_extraction import MinimalFCParameters
 from tsfresh.feature_extraction import ComprehensiveFCParameters
+from tsfresh.feature_extraction import EfficientFCParameters
 import pandas as pd
 import numpy as np
+import dask.dataframe as dd
 
 
 class FeatureExtractor:
     def __init__(self, default_fc_parameters: dict = None):
         if default_fc_parameters is None:
-            self.fc_parameters = ComprehensiveFCParameters()
+            self.fc_parameters = EfficientFCParameters()
         else:
             self.fc_parameters = default_fc_parameters
 
@@ -15,15 +18,13 @@ class FeatureExtractor:
         self,
         signal: np.ndarray,
         sample_id: int = 0,
-        kind: str = "signal"
     ) -> pd.DataFrame:
         """
         Extrai características de um único array 1D com tsfresh.
         """
         df = pd.DataFrame({
             "value": signal,
-            "id": sample_id,
-            "kind": kind,
+            "id": [sample_id] * len(signal),  # Repetindo o ID para cada ponto do sinal
             "time": np.arange(len(signal))
         })
 
